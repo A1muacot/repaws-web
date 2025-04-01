b6b576dc87a007af74f5e1110941c5b31872750d
unknown <ygrectiona1@gmail.com>
2025-04-01 22:26:06 +0700

    styling change

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const productImages = document.querySelectorAll(".product-card img");
   
-    productImages.forEach((img, index) => {
-      if (index < 3) {
-        img.style.cursor = "pointer";
+    productImages.forEach((img) => {
+        let i = 1;
         img.addEventListener("click", () => {
-          window.location.href = `/product/${index + 1}`;
+          window.location.href = `/product/${i}`;
+          i++;
+          if (i == 4) i = 1;
         });
-      }
     });
   });
